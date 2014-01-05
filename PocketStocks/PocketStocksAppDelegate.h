68ade0749f99e0e0fb8df8b12c277b25d687c9ac
Mike Wilcox <mwilcox56@gmail.com>
2014-01-05 16:37:42 -0500

    Initial Commit

---
+++ PocketStocks/PocketStocksAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PocketStocksAppDelegate.h
+//  PocketStocks
+//
+//  Created by Mike Wilcox on 05/01/2014.
+//  Copyright (c) 2014 Mike Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PocketStocksAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PocketStocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PocketStocks
+//
+//  Created by Mike Wilcox on 05/01/2014.
+//  Copyright (c) 2014 Mike Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PocketStocksAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PocketStocksAppDelegate class]));
+    }
+}
